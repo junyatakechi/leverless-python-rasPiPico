c5f53511e66884ed31d301c6355c5b4a8d9fc672
junyatakechi <takechi48j@gmail.com>
2023-07-02 11:54:52 +0900

    added wsad

--- main.py
+++ main.py
@@ -7,18 +7,34 @@ import digitalio
 
 k = Keyboard(usb_hid.devices)
 
-led = digitalio.DigitalInOut(board.GP15)
-led.direction = digitalio.Direction.OUTPUT
+button_w = digitalio.DigitalInOut(board.GP0)
+button_w.direction = digitalio.Direction.INPUT
+button_w.pull = digitalio.Pull.UP
 
-button = digitalio.DigitalInOut(board.GP13)
-button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
+button_a = digitalio.DigitalInOut(board.GP1)
+button_a.direction = digitalio.Direction.INPUT
+button_a.pull = digitalio.Pull.UP
+
+button_s = digitalio.DigitalInOut(board.GP2)
+button_s.direction = digitalio.Direction.INPUT
+button_s.pull = digitalio.Pull.UP
+
+button_d = digitalio.DigitalInOut(board.GP3)
+button_d.direction = digitalio.Direction.INPUT
+button_d.pull = digitalio.Pull.UP
+
+repeat_delay = 1 / 60  # 60fpsに基づく1フレームの時間
 
 while True:
-    if not button.value:
-        led.value = True
-        k.send(Keycode.F)
-        time.sleep(0.1)
-    else:
-        led.value = False
-        time.sleep(0.1)
+    if not button_w.value:
+        k.send(Keycode.W)
+        time.sleep(repeat_delay)
+    if not button_a.value:
+        k.send(Keycode.A)
+        time.sleep(repeat_delay)
+    if not button_s.value:
+        k.send(Keycode.S)
+        time.sleep(repeat_delay)
+    if not button_d.value:
+        k.send(Keycode.D)
+        time.sleep(repeat_delay)
